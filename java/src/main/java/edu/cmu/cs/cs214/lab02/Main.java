af679eeddb9136db0c8b4e7f7a685ad3fb6a58fd
Kangshuo Li <kangshul@andrew.cmu.edu>
2025-09-03 16:16:13 -0400

    improve encapsulation in java files

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,17 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Square(2.0);
 
         Renderer renderer = new Renderer(rectangle);
 
         renderer.draw();
+
+        Shape circle = new Circle(1.0);
+        Renderer renderer2 = new Renderer(circle);
+        renderer2.draw();
     }
 }
